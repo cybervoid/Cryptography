7a118c41b216b61512f7692fd522a7d22646cdd0
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-09-30 11:23:11 -0400

    basic "old" cryptography using letter shift

---
+++ CryptographyCsharp/CryptographyCsharp/Cryptography/Old/ShiftCipher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace CryptographyCsharp.Cryptography.Old
+{
+    public class ShiftCipher
+    {
+        public string PlainText { get; set; }
+        public string EncryptedText { get; set;  }
+        public string DecryptedText { get; set; }
+        public ShiftCipher()
+        {
+            
+        }
+
+		public ShiftCipher(string plainText)
+		{
+            this.PlainText = plainText;
+		}
+        public void Encrypt()
+        {
+            this.EncryptedText = Shift(this.PlainText);
+        }
+
+        public void Decrypt()
+        {
+            if (string.IsNullOrWhiteSpace(this.EncryptedText))
+               Encrypt();
+            this.DecryptedText = Decipher(this.EncryptedText);
+        }
+
+		public string Shift(string plaintext)
+        {
+            StringBuilder encryptedText = new StringBuilder();
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                char c = IntToChar(Key(CharToInt(plaintext[i])));
+                encryptedText.Append(c);
+            }
+            return encryptedText.ToString();
+        }
+
+        private int CharToInt(char c)
+        {
+            return (int)c;
+        }
+
+        public char IntToChar(int i)
+        {
+            return (char)i;
+        }
+
+        private int Key(int input)
+        {
+            return input + 5;
+        }
+
+        private int DeKey(int input)
+        {
+            return input - 5;
+        }
+
+        public string Decipher(string cipherText)
+        {
+			StringBuilder decryptedText = new StringBuilder();
+			for (int i = 0; i < cipherText.Length; i++)
+			{
+				char c = IntToChar(DeKey(CharToInt(cipherText[i])));
+				decryptedText.Append(c);
+			}
+			return decryptedText.ToString();
+        }
+    }
+}
--- CryptographyCsharp/CryptographyCsharp/Program.cs
+++ CryptographyCsharp/CryptographyCsharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CryptographyCsharp.Concepts;
+using CryptographyCsharp.Cryptography.Old;
 namespace CryptographyCsharp
 {
     class MainClass
@@ -7,8 +8,13 @@ namespace CryptographyCsharp
         public static void Main(string[] args)
         {
             //CryptographyCsharp.Concepts.Equivalence equivalence = new Concepts.Equivalence(22, 12);
-            Congruence congruence = new Congruence();
-            Console.WriteLine(congruence.IsCongruent(21, 7));
+            //Congruence congruence = new Congruence();
+            //Console.WriteLine(congruence.IsCongruent(21, 7));
+            ShiftCipher cs = new ShiftCipher("Hello World");
+            cs.Encrypt();
+            Console.WriteLine(cs.EncryptedText);
+            cs.Decrypt();
+            Console.WriteLine(cs.DecryptedText);
             Console.ReadLine();
 
         }
