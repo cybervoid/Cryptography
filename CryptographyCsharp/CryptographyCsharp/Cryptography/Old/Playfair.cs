b83512555eb7960cc9670442428966c4502d2bdf
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-10-18 06:43:30 -0400

    stub for playfair cipher

---
+++ CryptographyCsharp/CryptographyCsharp/Cryptography/Old/Playfair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CryptographyCsharp.Cryptography.Old
+{
+    public class Playfair
+    {
+        
+        public Playfair()
+        {
+        }
+
+        public void Run()
+        {
+            string message = "castle";
+            string key = "Playfair";
+        }
+
+        public void Grid(string key, string message)
+        {
+            int[,] grid = new int[5, 5];
+        }
+    }
+}
